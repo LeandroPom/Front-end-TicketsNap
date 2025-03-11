ae2eafca7d1cc274da6c1b86ddefc4bf3d1b6435
Maxip32 <piolattoemanuel@gmail.com>
2025-03-10 23:56:22 -0300

    css del paginado y verificacion de admin

--- src/components/AdminPanel/Pages/adminpanel.jsx
+++ src/components/AdminPanel/Pages/adminpanel.jsx
@@ -1,30 +1,60 @@
-import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-// import Sidebar from '../sidebar';
+import React, { useEffect, useState } from 'react';
+import { Routes, Route, useNavigate, Navigate } from 'react-router-dom';
 import Header from '../header';
 import Dashboard from './dashboard';
 import Events from './events';
-// import Seats from './seats';
 import Places from './places';
 import UsersManagement from './users';
-import "./estilospaneladm.css";
 import SoldTickets from './SoldTickets';
-
+import Swal from 'sweetalert2';
+import { useSelector } from 'react-redux';
+import './estilospaneladm.css';
 
 const AdminPanel = () => {
+  const user = useSelector((state) => state.user); // Obtener el usuario desde Redux
+  const [loading, setLoading] = useState(true); // Estado de carga
+  const [error, setError] = useState(null); // Estado de error (si lo necesitas)
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (user === null) {
+      // Si el usuario no está en Redux, continúa en estado de carga
+      setLoading(true);
+    } else {
+      // Una vez que el usuario esté cargado, verifica si es admin
+      if (!user.isAdmin) {
+        Swal.fire({
+          title: 'Acceso Denegado',
+          text: 'No tienes permisos para acceder a este panel.',
+          icon: 'error',
+        });
+        setError('Acceso Denegado'); // Opcional: puedes manejar el error
+        navigate("/")
+      } else {
+        setLoading(false); // Si el usuario es admin, termina el loading
+      }
+      
+    }
+  }, [user]); // Se ejecuta cada vez que cambia el usuario
+
+  if (loading) {
+    return <div>Loading...</div>; // Mostrar cargando mientras se obtiene el usuario
+  }
+
+  if (error) {
+    return <div>{error}</div>; // Mostrar error si el usuario no tiene permisos
+  }
+
   return (
     <div className="admin-panel">
-      {/* <Sidebar /> */}
       <div className="main-content">
         <Header />
         <Routes>
-        <Route path="/admin/dashboard" element={<Dashboard />} />
+          <Route path="/admin/dashboard" element={<Dashboard />} />
           <Route path="/admin/events" element={<Events />} />
-          {/* <Route path="/admin/seats" element={<Seats />} /> */}
           <Route path="/admin/places" element={<Places />} />
           <Route path="/admin/users" element={<UsersManagement />} />
           <Route path="/admin/tickets" element={<SoldTickets />} />
-        
           <Route path="*" element={<Navigate to="/admin/dashboard" />} />
         </Routes>
       </div>
